15081d0409bfe342be3efcd62a871a1341bf879d
spikey <spikey@rslp.org>
2024-02-24 11:16:19 -0500

    hello

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
-app.run()
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
